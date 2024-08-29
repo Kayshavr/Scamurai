8dc0bc909c0dc69b4a73c5bdd39622ff7f1acfe7
Kayshav <kayshav_renganathan@hotmail.com>
2024-08-29 22:34:27 +0800

    Add Link Validator

--- app.py
+++ app.py
@@ -2,9 +2,22 @@
 from dotenv import load_dotenv
 from streamlit_chat import message
 from PIL import Image
-import os, openai, streamlit as st, numpy as np
+import os, re, openai, streamlit as st, numpy as np
 
 load_dotenv()
+
+def check_link(web_link):
+    # Check if website link is valid
+    regex = re.compile(
+        r'^(?:http|ftp)s?://' # http:// or https://
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
+        r'localhost|' #localhost...
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
+        r'(?::\d+)?' # optional port
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    
+    return re.match(regex, web_link) is not None 
+
 
 st.title("Scamurai System")
 st.subheader("Enter input below:")
@@ -31,16 +44,13 @@ with st.container():
         st.header("Website Link")
         holder2 = st.empty()
         web_link = holder2.text_input("Enter Website Link", " ")
-        st.write("Website:", web_link)
-
-        if web_link is not " ":
+        
+        if check_link(web_link):
             st.text(web_link)
             holder2.empty()
-
-def chat_with_gpt():
-    prompt = st.chat_input("Please Enter Text input")
-    if prompt:
-        st.write(f"User has sent the following prompt: {prompt}")
+            st.write("Website: ", web_link)
+        else:
+            web_link = holder2.text_input("Please enter a valid web address",  " ")
 
 # def chat_with_gpt(prompt):
 
